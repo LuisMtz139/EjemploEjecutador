ed33592ae56256e2c2780d7fbbb6ef34860374d9
Luis Antonio Martinez Marroquin <luis@kali.luis>
2024-04-21 19:24:32 +0200

    funcional

--- ejemplo.py
+++ ejemplo.py
@@ -6,9 +6,9 @@ from tableError import TableError
 class VistaPrincipal:
     def __init__(self, master):
         self.master = master
-        self.master.title("Ejemplo de Vista")
+        self.master.title("Conalep-timbrado")
 
-        self.master.geometry("1100x650")
+        self.master.geometry("1800x1800")
 
         self.main_frame = tk.Frame(master)
         self.main_frame.pack(fill=tk.BOTH, expand=1)
@@ -18,22 +18,38 @@ class VistaPrincipal:
 
         inputs = ['Escenario', 'Leyenda', 'Crear']
         for i, input_text in enumerate(inputs):
-            tk.Label(self.inner_frame, text=input_text, font=("Arial", 12)).grid(row=0, column=i)
+            tk.Label(self.inner_frame, text=input_text, font=("Arial", 10)).grid(row=0, column=i)
             entry = tk.Entry(self.inner_frame, width=15)  
-            entry.grid(row=1, column=i, padx=20)  
+            entry.grid(row=1, column=i, padx=0)  
 
             if input_text == 'Crear':
                 self.inner_frame.grid_columnconfigure(i, weight=1)
 
         self.table = ttk.Treeview(self.inner_frame)
-        self.table['columns'] = ('#1', '#2', '#3')
+        self.table['columns'] = ('#1', '#2', '#3', '#4', '#5', '#6', '#7', '#8')
         self.table.column('#0', width=0, stretch=tk.NO)
         self.table.column('#1', width=150, stretch=tk.YES)  
         self.table.column('#2', width=150, stretch=tk.YES)
         self.table.column('#3', width=150, stretch=tk.YES)
-        self.table.heading('#1', text='Columna 1')
-        self.table.heading('#2', text='Columna 2')
-        self.table.heading('#3', text='Columna 3')
+        self.table.column('#4', width=150, stretch=tk.YES)
+        self.table.column('#5', width=150, stretch=tk.YES)
+        self.table.column('#6', width=150, stretch=tk.YES)
+        self.table.column('#7', width=150, stretch=tk.YES)
+        self.table.column('#8', width=150, stretch=tk.YES)
+        
+        self.table.heading('#1', text='ESCENARIOID')
+        self.table.heading('#2', text='QUINCENANO')
+        self.table.heading('#3', text='TIPONOMINA')
+        self.table.heading('#4', text='STATUS')
+        self.table.heading('#5', text='NOXMLSCANDIDATOS')
+        self.table.heading('#6', text='NOXMLSTIMBRADOS')
+        self.table.heading('#7', text='NOXMLSERRONEOS')
+        self.table.heading('#8', text='NOREVISION')
+        
+        # Estilo para las cabeceras de columna
+        style = ttk.Style()
+        style.configure("Treeview.Heading", font=("Arial", 10))
+        
         self.table.place(relx=0.5, rely=0.50, relwidth=0.95,relheight=0.7, anchor=tk.CENTER) 
 
         
--- tableError.py
+++ tableError.py
@@ -4,9 +4,8 @@ from tkinter import ttk
 class TableError:
     def __init__(self, master):
         self.master = master
-        self.master.title("Ejemplo de Vista")
-
-        self.master.geometry("1100x650")
+        self.master.title("Conalep-timbrado")
+        self.master.geometry("1800x1800")
         
         self.main_frame = tk.Frame(master)
         self.main_frame.pack(fill=tk.BOTH, expand=1)
@@ -14,13 +13,47 @@ class TableError:
         self.inner_frame = tk.Frame(self.main_frame, bd=2, relief=tk.SOLID)
         self.inner_frame.pack(fill=tk.BOTH, expand=1, padx=20, pady=20)  
 
+        # Nuevo recuadro dentro del recuadro existente con relleno adicional arriba y abajo
+        self.new_frame = tk.Frame(self.inner_frame, bd=2, relief=tk.SOLID)
+        self.new_frame.pack(fill=tk.BOTH, expand=1, padx=20, pady=(20, 70))
 
+        # Cuadro 1 - Datos txt
+        self.column1 = tk.Frame(self.new_frame, bd=2, relief=tk.SOLID)
+        self.column1.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
+        self.column1_label = tk.Label(self.column1, text="Datos txt", font=("Helvetica", 12, "bold"))
+        self.column1_label.pack(pady=10)
+
+        # Lista de elementos seleccionables
+        self.listbox = tk.Listbox(self.column1, height=5)
+        self.listbox.pack(fill=tk.BOTH, expand=True)
+        self.listbox.insert(1, "Dato 1")
+        self.listbox.insert(2, "Dato 2")
+        self.listbox.insert(3, "Dato 3")
+        self.listbox.bind("<<ListboxSelect>>", self.mostrar_error)
+
+        # Cuadro 2 - Error
+        self.column2 = tk.Frame(self.new_frame, bd=2, relief=tk.SOLID)
+        self.column2.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
+        self.column2_label = tk.Label(self.column2, text="Error", font=("Helvetica", 12, "bold"))
+        self.column2_label.pack(pady=10)
+        self.error_label = tk.Label(self.column2, text="", font=("Helvetica", 12))
+        self.error_label.pack(pady=10)
+
+        # Cuadro 3 - Contenido txt
+        self.column3 = tk.Frame(self.new_frame, bd=2, relief=tk.SOLID)
+        self.column3.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
+        self.column3_label = tk.Label(self.column3, text="Contenido txt", font=("Helvetica", 12, "bold"))
+        self.column3_label.pack(pady=10)
 
         self.button = tk.Button(self.main_frame, text="Test", command=self.cerrar)
         self.button.place(relx=0.1, rely=0.9, anchor=tk.CENTER)
 
     def cerrar(self):
         self.master.destroy()
+
+    def mostrar_error(self, event):
+        seleccion = self.listbox.get(self.listbox.curselection())
+        self.error_label.config(text=f"Dato seleccionado: {seleccion}")
 
 def main():
     root = tk.Tk()
