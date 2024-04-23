fd2379d914d2ed4af1f079fcd6b8bd935a4c4507
Luis Martinez <LuisMartinezMA12@gmail.com>
2024-04-22 23:11:18 -0600

    menu desplegable

--- ejemplo.py
+++ ejemplo.py
@@ -4,9 +4,11 @@ from tkinter import ttk
 from tkinter import filedialog
 import os
 import xml.etree.ElementTree as ET
+    
+import subprocess
 
 from tableError import TableError
-
+#http://localhost:1234/RocencranService/Generanomina/C:%7CFinalServiceQR%7CCNE781229BK4_TEST/TXT07/07/1112510074/N
 class VistaPrincipal:
     def __init__(self, master):
         self.master = master
@@ -14,7 +16,7 @@ class VistaPrincipal:
         
         tree = ET.parse('config.xml')
         root = tree.getroot()
-        self.path = root.find('Ruta').text
+        self.path = root.find('RutaCarpetas').text
 
 
         # Maximiza la ventana para que ocupe el 100% de la pantalla
@@ -27,13 +29,26 @@ class VistaPrincipal:
         self.inner_frame.pack(fill=tk.BOTH, expand=1, padx=20, pady=20) 
 
         inputs = ['Escenario Id', 'Quincena No.']
-        self.entries = {}  # Store references to entry widgets
+        self.entries = {}  # Almacena referencias a los widgets de entrada
         for i, input_text in enumerate(inputs):
             tk.Label(self.inner_frame, text=input_text, font=("Arial", 15)).grid(row=0, column=i, padx=10)
-            entry = tk.Entry(self.inner_frame, width=20, bg='light yellow', font=("Arial", 15), justify='center')  # Adjusted font size and centered text here
+            entry = tk.Entry(self.inner_frame, width=20, bg='light yellow', font=("Arial", 15), justify='center')  
             entry.grid(row=1, column=i, padx=45)
             self.entries[input_text] = entry
             
+            
+        options = ['Opción 1', 'Opción 2', 'Opción 3']  
+        self.dropdown = ttk.Combobox(self.inner_frame, values=options, font=("Arial", 15), state="readonly", style="Custom.TCombobox")
+        self.dropdown.grid(row=1, column=len(inputs), padx=10, sticky=tk.W)
+
+        # Definir el estilo del menú desplegable
+        self.inner_frame.style = ttk.Style()
+        self.inner_frame.style.theme_use("default")
+        self.inner_frame.style.configure("Custom.TCombobox", selectbackground=self.inner_frame.cget("background"), selectforeground="black", anchor="center", background="lightyellow")  # Configura el color del texto seleccionado en negro, el color de fondo del resaltado igual al fondo del menú desplegable, ancla el texto al centro y cambia el color de fondo del menú desplegable a amarillo claro
+
+        # Configurar el color de fondo de la lista desplegable
+        self.inner_frame.style.configure("Custom.TCombobox.Listbox", background="lightyellow")
+
 
         button = tk.Button(self.inner_frame, text='Nuevo', font=("Arial", 13), bg='light yellow')
         button.grid(row=1, column=len(inputs), padx=10, sticky=tk.E)
@@ -73,30 +88,44 @@ class VistaPrincipal:
         self.table.place(relx=0.5, rely=0.50, relwidth=0.99,relheight=0.7, anchor=tk.CENTER) 
 
         
-        self.button = tk.Button(self.main_frame, text="Test", command=self.cambiar_vista)
+        self.button = tk.Button(self.main_frame, text="Iniciar Porceso de timbrado",font=("Arial", 13), bg='light yellow',  command=self.cambiar_vista)
         self.button.place(relx=0.1, rely=0.9, anchor=tk.CENTER)
-        self.button.config(width=10, height=2, font=("Arial", 13))
-    
+        self.button.config(width=25, height=2, font=("Arial", 13))
+
     def crear_escenario(self):
-        # Get current date and time
         now = datetime.now()
 
-        # Convert to string
         now_str = now.strftime("%m%d%Y%H%M")
 
-        # Set this value in the 'Escenario Id' entry widget
-        self.entries['Escenario Id'].delete(0, tk.END)  # Remove any existing text
-        self.entries['Escenario Id'].insert(0, now_str)  # Insert the current date and time
-        os.makedirs(os.path.join(self.path, now_str), exist_ok=True)
+
+        self.entries['Escenario Id'].delete(0, tk.END)  
+        self.entries['Escenario Id'].insert(0, now_str)  
+
+
+        base_dir = os.path.join(self.path, now_str)
+        os.makedirs(base_dir, exist_ok=True)
+
+
+        os.makedirs(os.path.join(base_dir, 'erroneos'), exist_ok=True)
+        universo_dir = os.path.join(base_dir, 'universo')
+        os.makedirs(universo_dir, exist_ok=True)
+        os.makedirs(os.path.join(base_dir, 'timbrado'), exist_ok=True)
+
+        subprocess.run(['explorer', universo_dir])
+            
+        
+        
+        
+        
+        
     def cambiar_vista(self):
         self.new_window = tk.Toplevel(self.master)
         self.app = TableError(self.new_window)
 
-        # Obtener la posición de la ventana principal
         window_x = self.master.winfo_x()
         window_y = self.master.winfo_y()
 
-        # Establecer la posición de la nueva ventana en la misma ubicación
+
         self.new_window.geometry("+%d+%d" % (window_x, window_y))
 
     def cerrar(self):
