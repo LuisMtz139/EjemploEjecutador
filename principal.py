36a19f3635adfd5821fb4690765adadcb3b97eb5
LuisMtz <LuisMartinezMA12@gmail.com>
2024-04-27 11:30:12 -0600

    consumo_ funcional, redireccion

--- principal.py
+++ principal.py
@@ -6,7 +6,12 @@ import os
 import xml.etree.ElementTree as ET
 import subprocess
 import csv
+import urllib.parse
+import requests 
 from tableError import TableError
+import re
+
+
 #http://localhost:1234/RocencranService/Generanomina/C:%7CFinalServiceQR%7CCNE781229BK4_TEST/TXT07/07/1112510074/N
 class VistaPrincipal:
     def __init__(self, master):
@@ -36,11 +41,11 @@ class VistaPrincipal:
             self.entries[input_text] = entry
         
         # Título para el menú desplegable
-        tk.Label(self.inner_frame, text="Registro Patronal", font=("Arial", 15)).grid(row=0, column=len(inputs), padx=10, sticky=tk.W)
+        tk.Label(self.inner_frame, text="Registro Patronal", font=("Arial", 15)).grid(row=0, column=len(inputs), padx=20, sticky=tk.W)
 
-        options = ['IMSS', 'Opción 2', 'Opción 3']  
-        self.dropdown = ttk.Combobox(self.inner_frame, values=options, font=("Arial", 15), state="readonly", style="Custom.TCombobox")
-        self.dropdown.grid(row=1, column=len(inputs), padx=10, sticky=tk.W)
+        options = ['EXCEL NOMINA IMSS  54454646465', 'Opción 2', 'Opción 3']
+        self.dropdown = ttk.Combobox(self.inner_frame, values=options, font=("Arial", 15), state="readonly", style="Custom.TCombobox", width=30)
+        self.dropdown.grid(row=1, column=len(inputs), padx=20, sticky=tk.W)
 
 
         # Definir el estilo del menú desplegable
@@ -88,9 +93,14 @@ class VistaPrincipal:
         self.table.place(relx=0.5, rely=0.50, relwidth=0.99,relheight=0.7, anchor=tk.CENTER) 
 
         self.cargar_datos_escenario()
-        
-        self.button = tk.Button(self.main_frame, text="Iniciar Porceso de timbrado",font=("Arial", 13), bg='light yellow',  command=self.cambiar_vista)
+        self.button = tk.Button(self.main_frame, text="Iniciar Proceso de Timbrado", command=self.enviar_datos)
         self.button.place(relx=0.1, rely=0.9, anchor=tk.CENTER)
+        self.button.config(width=25, height=2, font=("Arial", 13))
+        
+        
+        
+        # self.button = tk.Button(self.main_frame, text="Iniciar Porceso de timbrado",font=("Arial", 13), bg='light yellow',  command=self.cambiar_vista)
+        # self.button.place(relx=0.1, rely=0.9, anchor=tk.CENTER)
         self.button.config(width=25, height=2, font=("Arial", 13))
 
 
@@ -136,18 +146,74 @@ class VistaPrincipal:
         # Añade el nuevo escenario a la tabla
         self.table.insert('', 'end', values=(now_str, quincena_no, tipo_nomina, 'STATUS', 'NOXMLSCANDIDATOS', 'NOXMLSTIMBRADOS', 'NOXMLSERRONEOS', 'NOREVISION'))
 
+    
                 
         
     def cambiar_vista(self):
+        # Imprimir los valores requeridos
+        escenario_id = self.entries['Escenario Id'].get()  # Obtener el valor actual de 'Escenario ID'
+        quincena_no = self.entries['Quincena No.'].get()  # Obtener el valor actual de 'Quincena No.'
+        tipo_nomina = self.dropdown.get()  # Obtener el valor seleccionado en el combobox para 'Tipo de Nómina'
+
+        print("Escenario ID:", escenario_id)
+        print("Quincena No.:", quincena_no)
+        print("Tipo de Nómina:", tipo_nomina)
+
+        # Aquí iría el resto del código para cambiar la vista o lo que originalmente hace cambiar_vista
         self.new_window = tk.Toplevel(self.master)
         self.app = TableError(self.new_window)
-
         window_x = self.master.winfo_x()
         window_y = self.master.winfo_y()
-
-
         self.new_window.geometry("+%d+%d" % (window_x, window_y))
 
+
+    def enviar_datos(self):
+        escenario_id = self.entries['Escenario Id'].get().strip()
+        quincena_no = self.entries['Quincena No.'].get().strip()
+        registro_patronal_text = self.dropdown.get().strip()
+        registro_patronal = ''.join(re.findall(r'\d+', registro_patronal_text))
+
+        escenario_id_encoded = urllib.parse.quote(escenario_id)
+        quincena_no_encoded = urllib.parse.quote(quincena_no)
+        registro_patronal_encoded = urllib.parse.quote(registro_patronal)
+
+        base_url = "http://localhost:1234/RocencranService/Generanomina/C:|Users|pssbo|Downloads|FinalServiceQRluisdesarrollo|FinalServiceQR|CNE781229BK4_TEST"
+        full_url = f"{base_url}/{escenario_id_encoded}/{quincena_no_encoded}/{registro_patronal_encoded}/S"
+
+        print("URL completa:", full_url)
+
+        try:
+            response = requests.get(full_url)
+            response.raise_for_status()  # Lanza un error para respuestas no exitosas
+            print("Respuesta recibida:", response.text)
+
+            # Verificar la carpeta de erróneos después de la petición
+            erroneos_dir = os.path.join(self.path, escenario_id, 'erroneos')
+            if os.listdir(erroneos_dir):  # Verificar si la carpeta contiene archivos
+                print("Se encontraron errores. Redireccionando a la vista de errores.")
+                self.mostrar_vista_errores()
+            else:
+                print("No se encontraron errores.")
+        except requests.RequestException as e:
+            print("Error al realizar la petición:", e)
+
+    def mostrar_vista_errores(self):
+        self.new_window = tk.Toplevel(self.master)
+        self.app = TableError(self.new_window)
+        window_x = self.master.winfo_x()
+        window_y = self.master.winfo_y()
+        self.new_window.geometry("+%d+%d" % (window_x, window_y))
+
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
     def cerrar(self):
         self.master.destroy()
 
