d225391c69a64563b9b0930439e07d760454a6c6
LuisMtz <LuisMartinezMA12@gmail.com>
2024-04-30 12:37:57 -0600

    funcona- hacer nuevos cambios

--- enviarTest.py
+++ enviarTest.py
@@ -1,19 +1,22 @@
+import os
 import urllib.parse
 import requests
-import os
 
 class DataSender:
-    def __init__(self, escenario_id, quincena_no, registro_patronal, path, mostrar_vista_errores):
-        self.escenario_id = escenario_id
-        self.quincena_no = quincena_no
-        self.registro_patronal = registro_patronal
+    def __init__(self, entries, dropdown, path, mostrar_vista_errores):
+        self.entries = entries
+        self.dropdown = dropdown
         self.path = path
         self.mostrar_vista_errores = mostrar_vista_errores
 
     def enviar_datos(self):
-        escenario_id_encoded = urllib.parse.quote(self.escenario_id)
-        quincena_no_encoded = urllib.parse.quote(self.quincena_no)
-        registro_patronal_encoded = urllib.parse.quote(self.registro_patronal)
+        escenario_id = self.entries['Escenario Id'].get().strip()
+        quincena_no = self.entries['Quincena No.'].get().strip()
+        registro_patronal = self.dropdown.get().strip()
+
+        escenario_id_encoded = urllib.parse.quote(escenario_id)
+        quincena_no_encoded = urllib.parse.quote(quincena_no)
+        registro_patronal_encoded = urllib.parse.quote(registro_patronal)
 
         base_url = "http://localhost:1234/RocencranService/Generanomina/C:|Users|pssbo|Downloads|FinalServiceQRluisdesarrollo|FinalServiceQR|CNE781229BK4_TEST"
         full_url = f"{base_url}/{escenario_id_encoded}/{quincena_no_encoded}/{registro_patronal_encoded}/S"
@@ -26,7 +29,7 @@ class DataSender:
             print("Respuesta recibida:", response.text)
 
             # Verificar la carpeta de erróneos después de la petición
-            erroneos_dir = os.path.join(self.path, self.escenario_id, 'erroneos')
+            erroneos_dir = os.path.join(self.path, escenario_id, 'erroneos')
             if os.listdir(erroneos_dir):  # Verificar si la carpeta contiene archivos
                 print("Se encontraron errores. Redireccionando a la vista de errores.")
                 self.mostrar_vista_errores()
